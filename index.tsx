d748001856d53494eac973427b924324a037e4b1
Alexey Kosyanov <alexey.kosyanov@ottofeller.com>
2021-07-15 17:58:33 +0300

    Add useIntersectionObserver hook

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+export {useIntersectionObserver} from './hooks/use-intersection-observer'
 export {useClickOutsideEffect} from './hooks/use-click-outside-effect'
 export {useIsClientSide} from './hooks/use-is-client-side'
 export {usePopup} from './hooks/use-popup'
